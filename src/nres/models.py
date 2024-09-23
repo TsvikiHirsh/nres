3df5192bb7598e4f5adee5f814ba63a16898e0d2
Tsviki Y. Hirsh <tsviki@soreq.gov.il>
2024-09-23 14:54:06 +0000

    fix model representation

--- src/nres/models.py
+++ src/nres/models.py
@@ -2,41 +2,17 @@ import lmfit
 import numpy as np
 import nres.utils as utils
 from nres.response import Response
+from nres.data import Data
 import pandas
+import matplotlib.pyplot as plt
 
-
-
-class TransmissionModelResult(lmfit.model.ModelResult):
-    def __init__(self, model, params, **kwargs):
-        """
-        Custom result class for TransmissionModel.
-        """
-        super().__init__(model, params, **kwargs)
-
-    def plot(self, **kwargs):
-        """
-        Custom plot method that includes a legend with isotopes.
-        
-        Parameters:
-        - kwargs: keyword arguments for the plot function.
-        
-        Returns:
-        - matplotlib figure and axis.
-        """
-        fig, ax = plt.subplots()
-        # Plot the data and the best-fit model
-        self.plot_fit(ax=ax, **kwargs)
-        
-        # Create custom legend based on isotopes
-        legend_text = "Isotopes: "
-        ax.legend([legend_text], loc='best')
-
-        return fig, ax
 
 class TransmissionModel(lmfit.Model):
-    def __init__(self, cross_section, vary_weights=False, 
+    def __init__(self, cross_section, 
+                 vary_weights=False, 
                  vary_background=False, 
-                 vary_tof=False,**kwargs):
+                 vary_tof=False,
+                 **kwargs):
         """
         Initialize the TransmissionModel, a subclass of lmfit.Model.
 
@@ -47,6 +23,8 @@ class TransmissionModel(lmfit.Model):
             If True, allows the isotope weights to vary during fitting.
         - vary_background: bool, optional (default=False)
             If True, allows the background parameters (b0, b1, b2) to vary during fitting.
+        - vary_tof: bool, optional (default=False)
+            If True, allows the tof parameters (L0, t0) to vary during fitting.
         - kwargs: dict
             Additional keyword arguments for background parameters, such as `b0`, `b1`, and `b2`.
         """
@@ -62,9 +40,14 @@ class TransmissionModel(lmfit.Model):
                             vary=vary_weights)
 
         # Initialize background parameters with provided values or defaults
-        bg_args = {"b0": kwargs.get("b0", 1e-5), "b1": kwargs.get("b1", 1e-5), "b2": kwargs.get("b2", 1e-5)}
+        bg_args = {"b0": kwargs.get("b0", 0.), "b1": kwargs.get("b1", 0.), "b2": kwargs.get("b2", 0.)}
         for b in bg_args:
             self.params.add(b, value=bg_args[b], vary=vary_background)
+
+        # Initialize tof parameters with provided values or defaults
+        tof_args = {"L0": kwargs.get("L0", 1.), "t0": kwargs.get("t0", 0.)}
+        self.params.add("L0", value=tof_args["L0"], min=0.5, max= 1.5, vary=vary_tof)
+        self.params.add("t0", value=tof_args["t0"], min=-5.0e-9, max= 5.0e-9, vary=vary_tof)
 
         # set the n parameter as fixed
         if self.cross_section.n:
@@ -103,7 +86,7 @@ class TransmissionModel(lmfit.Model):
         E = utils.time2energy(tof+dtof,self.cross_section.L)
 
         # Background polynomial
-        bg = b0 + b1 * np.sqrt(E) + b2 * np.sqrt(E)
+        bg = b0 + b1 * np.sqrt(E) + b2 / np.sqrt(E)
         
         response = self.response.function(**response_kw)
         weights = [response_kw.get(key,self.cross_section.isotopes[key]) for key in self.cross_section.isotopes]
@@ -129,14 +112,42 @@ class TransmissionModel(lmfit.Model):
             The result of the fit.
         """
         # self.cross_section.set_energy_range(emin,emax)
-        if type(data) == pandas.DataFrame:
+        if isinstance(data,pandas.DataFrame):
             data = data.query(f"{emin}<energy<{emax}")
+            weights = kwargs.get("weights",1./data["err"].values)
+            fit_result = super().fit(data["trans"].values, params=params or self.params, weights=weights, E=data["energy"].values, **kwargs)
+        elif isinstance(data,Data):
+            data = data.table.query(f"{emin}<energy<{emax}")
             weights = kwargs.get("weights",1./data["err"].values)
             fit_result = super().fit(data["trans"].values, params=params or self.params, weights=weights, E=data["energy"].values, **kwargs)
         else:
             # Perform the fit using the parent class's fit method
             fit_result = super().fit(data, params=params or self.params, **kwargs)
+        self.fit_result = fit_result
+        fit_result.plot = self.plot
+
         # return TransmissionModelResult(fit_result, params or self.params)
         return fit_result
+    
+    def plot(self,**kwargs):
+        fig, ax = plt.subplots(2,1,sharex=True,height_ratios=[3.5,1],figsize=(6,5))
+        energy = self.fit_result.userkws["E"]
+        data = self.fit_result.data
+        err = 1./self.fit_result.weights
+        best_fit = self.fit_result.best_fit
+        residual = self.fit_result.residual
+        color = kwargs.pop("color","seagreen")
+        ecolor = kwargs.pop("ecolor","0.8")
+        ms = kwargs.pop("ms",2)
+        ax[0].errorbar(energy,data,err,marker="o",color=color,ms=ms,zorder=-1,ecolor=ecolor)  
+        ax[0].plot(energy,best_fit,color="0.2") 
+        ax[0].set_ylabel("Transmission")
+        ax[0].set_title(self.cross_section.name)
+        ax[1].plot(energy,residual,color=color)
+        ax[1].set_ylabel("residuals [1σ]")
+        ax[1].set_xlabel("Energy [eV]")
+        plt.subplots_adjust(hspace=0.05)
+        ax[0].legend(["Best fit","Data"],title=f"χ$^2$: {self.fit_result.redchi:.2f}")
+        return ax
     
 
