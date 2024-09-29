9d0a1cbc2a9c427922153cc3ead6538750fe46c6
Tsviki Y. Hirsh <tsviki@soreq.gov.il>
2024-09-29 12:33:18 +0000

    add readthedocs

---
+++ docs/source/conf.py
@@ -0,0 +1,47 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'nres'
+copyright = '2024, Tsviki Y. Hirsh'
+author = 'Tsviki Y. Hirsh'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+
+# Add any Sphinx extension module names here
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.viewcode',
+]
+
+# The theme to use for HTML and HTML Help pages
+html_theme = 'sphinx_rtd_theme'
+
+# The name of the Pygments (syntax highlighting) style to use
+pygments_style = 'sphinx'
---
+++ docs/source/nres.rst
@@ -0,0 +1,22 @@
+nres package
+=============
+
+Submodules
+----------
+
+.. toctree::
+   :maxdepth: 4
+
+   cross_section
+   data
+   models
+   response
+   utils
+
+Module contents
+---------------
+
+.. automodule:: nres
+   :members:
+   :undoc-members:
+   :show-inheritance:
