82e9de33d747164b3716f0cdb6eb1773c2b5e091
Tsviki Y. Hirsh <tsviki@soreq.gov.il>
2025-01-04 18:02:12 +0000

    bump minor version 0.2.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'nres'
 copyright = '2024, Tsviki Y. Hirsh'
 author = 'Tsviki Y. Hirsh'
-release = '0.2.1'
+release = '0.2.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
